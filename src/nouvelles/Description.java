0c4768ffe274fd910da24486a7e822a6670633b0
lucieandres <lucie.andres@etu.unice.fr>
2021-10-13 14:45:35 +0200

    description

--- src/nouvelles/Description.java
+++ src/nouvelles/Description.java
@@ -1,5 +1,20 @@
 package nouvelles;
 
+import java.time.LocalDate;
+
 public class Description {
 
+	LocalDate departDate;
+	String departPlace;
+	String arrivalPlace;
+	int duration;
+	
+	public Description (LocalDate departDate, String departPlace, String arrivalPlace, int duration) {
+		this.departDate = departDate;
+		this.departPlace = departPlace;
+		this.arrivalPlace = arrivalPlace;
+		this.duration = duration;
+	}
+	
+	
 }
