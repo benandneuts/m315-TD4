c5da1f5948564f753d0d7c8abba85a9bb7ddf79b
Benjamin Neuville <b.neuville@outlook.fr>
2021-10-17 15:54:24 +0200

    a revoir : methode service

--- src/nouvelles/Service.java
+++ src/nouvelles/Service.java
@@ -2,19 +2,20 @@ package nouvelles;
 
 import java.util.*;
 
+import td4.core.PayingItem;
 import td4.core.Product;
 
 public interface Service {
 	ArrayList<Trip> trips = new ArrayList<>();
 	
-	public default Product find(Description d) {
+	public default PayingItem find(Description d) {
 		for(int i = 0 ; i < trips.size(); i++) {
 			for(int j = 0 ; j < trips.get(i).getProducts().size(); j++) {
 				if( trips.get(i).getProducts().get(j) == d ) {
-					return trips.get(i).getProducts().get(j)
+					return trips.get(i).getProducts().get(j);
 				}
 			}
 		}
-		
+		return null;
 	}
 }
