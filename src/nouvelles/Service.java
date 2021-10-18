fc92cc3e278f9d7f76af4a72b7c0a9ee3192983f
Benjamin Neuville <b.neuville@outlook.fr>
2021-10-18 09:30:20 +0200

    modif et supp des class

--- src/nouvelles/Service.java
+++ src/nouvelles/Service.java
@@ -3,19 +3,19 @@ package nouvelles;
 import java.util.*;
 
 import td4.core.PayingItem;
-import td4.core.Product;
+import td4.core.PayingItem;
 
 public interface Service {
-	ArrayList<Trip> trips = new ArrayList<>();
 	
-	public default PayingItem find(Description d) {
-		for(int i = 0 ; i < trips.size(); i++) {
-			for(int j = 0 ; j < trips.get(i).getProducts().size(); j++) {
-				if( trips.get(i).getProducts().get(j) == d ) {
-					return trips.get(i).getProducts().get(j);
-				}
-			}
-		}
-		return null;
-	}
+	public PayingItem find(Description d); 
+//	{
+//		for(int i = 0 ; i < trips.size(); i++) {
+//			for(int j = 0 ; j < trips.get(i).getProducts().size(); j++) {
+//				if( trips.get(i).getProducts().get(j)/*description ??*/ == d ) {
+//					return trips.get(i).getProducts().get(j);
+//				}
+//			}
+//		}
+//		return null;
+//	}
 }
