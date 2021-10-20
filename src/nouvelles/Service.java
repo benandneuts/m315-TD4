958d2dc92ee26006e24a27cea2b7d82830112960
lucieandres <lucie.andres@etu.unice.fr>
2021-10-20 13:53:08 +0200

    Update Service.java

--- src/nouvelles/Service.java
+++ src/nouvelles/Service.java
@@ -8,14 +8,16 @@ import td4.core.PayingItem;
 public interface Service {
 	
 	public PayingItem find(Description d); 
-//	{
-//		for(int i = 0 ; i < trips.size(); i++) {
-//			for(int j = 0 ; j < trips.get(i).getProducts().size(); j++) {
-//				if( trips.get(i).getProducts().get(j)/*description ??*/ == d ) {
-//					return trips.get(i).getProducts().get(j);
-//				}
-//			}
-//		}
-//		return null;
-//	}
+	/*
+	{
+		for(int i = 0 ; i < trips.size(); i++) {
+			for(int j = 0 ; j < trips.get(i).getProducts().size(); j++) {
+				if( trips.get(i).getProducts().get(j)description ?? == d ) {
+					return trips.get(i).getProducts().get(j);
+				}
+			}
+		}
+		return null;
+	}
+	*/
 }
